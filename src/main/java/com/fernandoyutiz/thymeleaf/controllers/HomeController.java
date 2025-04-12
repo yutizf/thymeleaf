fc5da0a898548148147436b90214cac84bb629d1
Fernando Yutiz <yutizf@hotmail.com>
2025-04-12 12:07:19 -0300

    Agregado de entornos

--- src/main/java/com/fernandoyutiz/thymeleaf/controllers/HomeController.java
+++ src/main/java/com/fernandoyutiz/thymeleaf/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.fernandoyutiz.thymeleaf.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,15 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    @Value("${entorno}")
+    String entorno;
+
     @GetMapping("/hello")
     public String hello(Model model){
         Map<String,String> mapa= new HashMap<>();
         mapa.put("saludo","Buenos dias");
         mapa.put("nombre", "Fernando");
+        mapa.put("entorno", entorno);
         model.addAttribute("mapa",mapa);
         return "hello";
     }
